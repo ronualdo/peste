5bd3af3d9d4cb75a2abd237610989a1ce2723b70
ronualdo maciel <raxmac@gmail.com>
2020-02-27 21:19:56 -0300

    Makes movies visualization group sessions by time

--- apps/peste_api/lib/peste_api_web/peste_view.ex
+++ apps/peste_api/lib/peste_api_web/peste_view.ex
@@ -6,8 +6,15 @@ defmodule PesteApiWeb.PesteView do
 
   def format(%Movie{sessions: sessions, title: title}) do
     sessions
+    |> Enum.group_by(fn session -> session.type end)
+    |> Enum.map(fn {type, sessions} -> %{type: type, start_times: merge_start_times(sessions)} end)
     |> Enum.map(fn session -> "#{title} #{session.type} [#{parse_start_times(session.start_times)}]" end)
     |> Enum.join(~s(\n))
+  end
+
+  def merge_start_times(sessions) do
+    sessions
+    |> Enum.reduce([], fn session, acc -> acc ++ session.start_times end)
   end
 
   def parse_start_times(start_times), do: Enum.join(start_times, ", ")
--- apps/peste_api/test/peste_api_web/peste_view_test.exs
+++ apps/peste_api/test/peste_api_web/peste_view_test.exs
@@ -16,13 +16,28 @@ defmodule PesteApiWeb.PesteViewTest do
 
       result = PesteView.format(movie)
 
-      assert result == ~s(test movie Legendado [14:23, 21:45]\ntest movie Dublado 3D [14:23, 21:45, 23:40])
+      assert result == ~s(test movie Dublado 3D [14:23, 21:45, 23:40]\ntest movie Legendado [14:23, 21:45])
     end
 
     test "returns an empty string for nil parameters" do
       result = PesteView.format(nil)
 
       assert result == ""
+    end
+
+    test "movies string representation groups duplicated session types" do
+      movie = %Movie{
+        title: "test movie",
+        sessions: [
+          %{type: "Legendado", start_times: ["14:23", "21:45"]},
+          %{type: "Dublado 3D", start_times: ["14:23", "21:45", "23:40"]},
+          %{type: "Legendado", start_times: ["22:45"]}
+        ]
+      } 
+
+      result = PesteView.format(movie)
+
+      assert result == ~s(test movie Dublado 3D [14:23, 21:45, 23:40]\ntest movie Legendado [14:23, 21:45, 22:45])
     end
   end
 end
