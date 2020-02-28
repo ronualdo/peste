9592a3b91839d9d3231d051b15c68fe1503445ed
ronualdo maciel <raxmac@gmail.com>
2020-02-27 21:45:35 -0300

    Refactor peste view to improve readability

--- apps/peste_api/lib/peste_api_web/peste_view.ex
+++ apps/peste_api/lib/peste_api_web/peste_view.ex
@@ -6,15 +6,25 @@ defmodule PesteApiWeb.PesteView do
 
   def format(%Movie{sessions: sessions, title: title}) do
     sessions
-    |> Enum.group_by(fn session -> session.type end)
-    |> Enum.map(fn {type, sessions} -> %{type: type, start_times: merge_start_times(sessions)} end)
-    |> Enum.map(fn session -> "#{title} #{session.type} [#{parse_start_times(session.start_times)}]" end)
+    |> Enum.group_by(&session_type/1)
+    |> Enum.map(&merge_sessions_by_type/1)
+    |> Enum.map(fn session -> format_session(session, title) end)
     |> Enum.join(~s(\n))
   end
 
-  def merge_start_times(sessions) do
+  defp session_type(session), do: session.type
+
+  defp merge_sessions_by_type({type, sessions}) do
+    %{type: type, start_times: merge_start_times(sessions)}
+  end
+
+  defp merge_start_times(sessions) do
     sessions
     |> Enum.reduce([], fn session, acc -> acc ++ session.start_times end)
+  end
+
+  defp format_session(%{type: type, start_times: start_times}, movie_title) do
+    "#{movie_title} #{type} [#{parse_start_times(start_times)}]"
   end
 
   def parse_start_times(start_times), do: Enum.join(start_times, ", ")
